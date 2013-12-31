7e57914f822fc44b132671d3f8f188f65f8c875e
Rose Yu <yuqi.rose@hotmail.com>
2013-12-31 14:44:43 -0800

    Updated driverFoursquare

--- driverFoursquare.m
+++ driverFoursquare.m
@@ -15,9 +15,15 @@ series = venue_checkin_counts;
 Loc(:,1) = venue_IDs;
 Loc(:,2:3) = venue_loc;
 
-N = 1000;
 
+checkin_sum = sum(venue_checkin_counts,2);
+[sorted_checkin, order]  = sort(checkin_sum,'descend');
 
+series = series(order,:);
+Loc = Loc(order,:);
+%%
+
+N = 100;
 TLam = 100;
 lambda = [1, 1e-5];
 nLag = 5;
@@ -26,6 +32,7 @@ Ttest = 10;
 index{1} = nLag+1:T-Ttest;
 index{2} = T-Ttest+1:T;
 Lambda_2 = lambda(2);
+
 %%
 grad = {@gradPoisson, 'Poisson'};
 [Sol, err, normerr] = sparseGLARP(series(1:N,:), Lambda_2, nLag, index, grad);
