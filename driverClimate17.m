a976f2e095d23db7d37ccc73809de4ffe4ca399d
Taha Bahadori <taha_bahadori@yahoo.com>
2014-01-01 12:59:22 -0800

    climateDriver is working.

--- driverClimate17.m
+++ driverClimate17.m
@@ -1,2 +1,23 @@
-% drive for climate18_cell.mat to compare the shared structure of
-% Sparse+Low Rank
+% Find 17 dependency matrices
+clc
+clear
+
+load climateP17.mat
+
+global verbose
+verbose = 1;
+addpath(genpath('./'))
+
+nType = length(series);
+Sol = cell(nType, 1);
+nLag = 1;
+lambda = 1e-4;
+index{1} = nLag+1:size(series{1}, 2);
+index{2} = [];
+grad = {@gradGaussian, 'Gaussian'};
+for i = 1:nType
+    [tempSol, err, normerr] = sparseGLARP(series{i}, lambda, nLag, index, grad);
+    Sol{i} = tempSol{1};
+end
+
+save('climate17Results.mat', 'Sol')
