7d046e6eab397e4de95872a348729f2b46904df5
Rose Yu <yuqi.rose@hotmail.com>
2014-01-20 13:40:00 -0800

    Add path

--- testGeo.m
+++ testGeo.m
@@ -1,5 +1,6 @@
 clear;
 clc;
+addpath(genpath('GeoML'));
 lambda = 216;
 beta = 1e-2;
 thres = 1e-4;
@@ -13,10 +14,11 @@ rank_Mix = [];
 rank_Mix_geo = [];
 
 for pr = fliplr([0.1:0.1:0.5])
+    fprintf('start Geo-spatio Multi-linear Learning \n');
+    [W_Cvx_geo, W_Mix_geo,~,~] = Kriging_Learn(data, lambda, beta ,pr);
     fprintf('start Multi-task Multi-linear Learning \n');
     [W_Cvx,W_Mix,~,~] = MLMTL_Learn(data, lambda, beta, pr);
-	fprintf('start Geo-spatio Multi-linear Learning \n');
-    [W_Cvx_geo, W_Mix_geo,~,~] = Kriging_Learn(data, lambda, beta ,pr);
+
     
     [~,~,~,ratio] = tensorModeRank(reshape(W_Cvx, dimModes));
     rank_Cvx = [rank_Cvx, ratio];
