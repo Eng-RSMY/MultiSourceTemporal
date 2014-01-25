bdd898b8d99a4187b4b8baf7d763a3f407891cdb
Taha <taha_bahadori@yahoo.com>
2014-01-25 15:30:54 -0800

    Housekeeping: moving the synth scripts to a new folder.

---
+++ synthScripts/runGreedies.m
@@ -0,0 +1,19 @@
+% Run Greedies
+
+global verbose
+verbose = 0;
+tLen = [10, 50, 100, 200];
+qFor = cell(4, 1);
+qOrth = cell(4, 1);
+for t = 1:length(tLen)
+    qFor{t} = zeros(10, 6);
+    qOrth{t} = qFor{t};
+    parfor i = 1:10
+        name = sprintf('synth%d_%d.mat', tLen(t), i);
+        [qForTemp(i, :), qOrthTemp(i, :)] = runSynthGreedy(name);
+    end
+    qFor{t} = qForTemp;
+    qOrth{t} = qOrthTemp;
+    save('greedyResultsSynth.mat', 'qFor', 'qOrth')
+    disp(t)
+end
