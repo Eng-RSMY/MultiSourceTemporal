20fe0d8edc0631201c1610034225295cfb48e0d6
Rose Yu <yuqi.rose@hotmail.com>
2014-01-25 18:59:55 -0800

    Added MTGL for FSQ

---
+++ MLMTL/MTL/run_MTGL_FSQ.m
@@ -0,0 +1,28 @@
+clear;
+clc;
+addpath(genpath('~/Documents/MATLAB/MultiSourceTemporal'));
+load 'tensor_checkin_small';
+ratio = 0.1;
+lambdas = [-3,3,10];
+[Dat_eval , Dat_test] = MTGL_Datpre(series,ratio); 
+
+fprintf('Running Lasso\n');
+
+[W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'Lasso',lambdas);
+Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
+fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
+save('MTGL_Lasso_FSQ.mat','Quality','train_time','opt_lambda','W');
+
+fprintf('Running L21\n');
+[W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'L21',lambdas);
+Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
+fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
+save('MTGL_L21_FSQ.mat','Quality','train_time','opt_lambda','W');
+
+fprintf('Running Dirty\n');
+[W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'Dirty',lambdas);
+Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
+fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
+save('MTGL_Dirty_FSQ.mat','Quality','train_time','opt_lambda','W');
+
+exit;
