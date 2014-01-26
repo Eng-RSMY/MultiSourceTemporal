740eb1a510fb182a564f8f91e29a2446d402f992
Rose Yu <yuqi.rose@hotmail.com>
2014-01-26 12:47:16 -0800

    Update script for climateP3

--- MLMTL/MTL/run_MTGL_Climate.m
+++ MLMTL/MTL/run_MTGL_Climate.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 addpath(genpath('~/Documents/MATLAB/MultiSourceTemporal'));
-load 'climateP17';
+load 'climateP3';
 ratio = 0.1;
 lambdas = logspace(-3,3,10);
 [Dat_eval , Dat_test] = MTGL_Datpre(series,ratio); 
@@ -11,16 +11,16 @@ fprintf('Running Lasso\n');
 [W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'Lasso',lambdas);
 Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
 fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
-save('MTGL_Lasso_climate.mat','Quality','train_time','opt_lambda','W');
+save('MTGL_Lasso_climateP3.mat','Quality','train_time','opt_lambda','W');
 
 fprintf('Running L21\n');
 [W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'L21',lambdas);
 Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
 fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
-save('MTGL_L21_climate.mat','Quality','train_time','opt_lambda','W');
+save('MTGL_L21_climateP3.mat','Quality','train_time','opt_lambda','W');
 
 fprintf('Running Dirty\n');
 [W,opt_lambda,train_time] = MTGL_Crosval(Dat_eval.X, Dat_eval.Y, 'Dirty',lambdas);
 Quality = MTGL_Test(Dat_test.X, Dat_test.Y, W);
 fprintf('RMSE %d, NRMSE %d, Rank %d, Time %d\n', Quality.RMSE, Quality.NRMSE,Quality.Rank, train_time);
-save('MTGL_Dirty_climate.mat','Quality','train_time','opt_lambda','W');
+save('MTGL_Dirty_climateP3.mat','Quality','train_time','opt_lambda','W');
