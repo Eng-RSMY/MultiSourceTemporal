73dff42eb2b098ea3de4d13e86316af6c486ea0a
Rose Yu <yuqi.rose@hotmail.com>
2014-05-29 14:52:02 -0700

    save current

--- forecasting/convex_forecasting.m
+++ forecasting/convex_forecasting.m
@@ -1,6 +1,6 @@
-function [ output_args ] = convex_forecasting( X,  Sim, lambda, beta, mu )
+function [ output_args ] = convex_forecasting( X,  Sim, lambda, beta, mu, nLag )
 %CONVEX_FORECASTING Summary of this function goes here
-%   Detailed explanation goes here
+%   Detailed explanation goes here?X P x T x M
 
 global verbose;
 verbose = 1;
@@ -9,18 +9,30 @@ maxIter = 500;
 Dims  = size(X);
 nModes = length(Dims);
 nLoc  = Dims(1);
+nTime = Dims(2);
 nTask = Dims(3);
 thres = 1e-6;
 
 % intialize 
-W = zeros(Dims);
+Dims_bar = [nLoc, nLoc*nLag, nTask];
+W = zeros(Dims_bar);
 Z = cell(nModes,1);
 C = cell(nModes,1);
 
 
 for n = 1: nModes
-    Z{n} = zeros(Dims);
-    C{n} = zeros(Dims);
+    Z{n} = zeros(Dims_bar);
+    C{n} = zeros(Dims_bar);
+end
+
+% construct X_bar
+Xbar = zeros(nLoc*nLag, nTime-nLag, nTask);
+Y = zeros(nLoc,nTime-nLag, nTask);
+for t = 1:nTasks
+    for l = 1:nLag
+        Xbar((l-1)*nLoc+1:l*nLoc,: ,t) = X(:,l:nTime-nLag+l,t);       
+    end
+    Y(:,:,t) = X(:,nLag+1:nTime,t);
 end
 
 
@@ -29,7 +41,7 @@ end
 L = diag(sum(Sim, 2)) - Sim;
 
 fs =[];
-fval_old = obj_cov_kriging(X, W, Z, C, beta, lambda);
+fval_old = obj_forecasting(Xbar,Y, W, L, Z, C, beta, lambda);
 for iter = 1:maxIter
     CnSum = zeros(Dims);
     ZnSum = zeros(Dims);
@@ -39,7 +51,7 @@ for iter = 1:maxIter
     end
     % Solve W 
     for t = 1:nTask
-        W(:,:,t) = ((1+nModes*beta)* eye(nLoc)+ mu*2*L)\( X(:,:,t)+ CnSum(:,:,t)+ beta* ZnSum(:,:,t) ) ;
+        W(:,:,t) = (Xbar*Xbar'+ nModes*beta* eye(nLoc)+ mu*2*L*(Xbar*Xbar'))\(Xbar(:,:,t)*Y(:,:,t)+ CnSum(:,:,t)+ beta* ZnSum(:,:,t) ) ;
 
     end
     % Optimizing over B    
@@ -55,7 +67,7 @@ for iter = 1:maxIter
         C{n}=C{n} -  beta*(W-Z{n});
     end
     
-    fval = obj_cov_kriging(X, W, Z, C, beta, lambda);
+    fval = obj_forecasting(Xbar, Y, W,L, Z, C, beta, lambda);
     if(abs(fval-fval_old)/fval < thres)
         break;
     end
@@ -70,12 +82,13 @@ W = W(idx_Missing,:,:);
 
 end
 
-function val =  obj_cov_kriging(X, W, Z, C, beta, lambda)
+function val =  obj_forecasting(X, Y, W, L,Z, C, beta, lambda)
 val = 0;
 nTasks = size(W,3);
 nModes = ndims (W);
 for t = 1:nTasks
-    val = val + 0.5*norm(W(:,:,t)-X(:,:,t),'fro')^2;
+    val = val + 0.5*norm(W(:,:,t)*X-Y(:,:,t),'fro')^2;
+    val = val  + trace(W(:,:,t)*X(:,:,t)'*L);
 end
 for n = 1:nModes
     tmp = (W-Z{n});
