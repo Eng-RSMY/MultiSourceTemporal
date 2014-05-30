0c679c153239f286f3a095cdbb4174ee5f659136
Taha <taha_bahadori@yahoo.com>
2014-05-30 15:29:45 -0700

    Sync main tuning for forecast.

--- Greedy/preForecast.m
+++ Greedy/preForecast.m
@@ -3,46 +3,56 @@ clear
 
 addpath(genpath('../'))
 load('../data/climateP17.mat')
-load('../data/climateP17_missIdx.mat')
-% load('../data/climateP4.mat')
-% load('../data/synth/datasets/synth200_9.mat')
-% load('../data/Foursquare/norm_4sq.mat')
 
 % For Euclidian
 % sigma = 2;
 % mu = 18.3486;
 % For Harversine
 sigma = 0.1;  % Should be selected in a way that effectively few neighbors get weights
-mu = 5;
+
 % nTask = length(series);
 [nLoc, tLen] = size(series{1});
 
 global verbose
 verbose = 1;
 global evaluate
-evaluate = 2;
-
+evaluate = 3;
 
 % sim =  euclidSim(locations, sigma);
 sim = haverSimple(locations, sigma);
 sim = sim/(max(sim(:)));       % The goal is to balance between two measures
 
-max_iter = 151;
-quality = zeros(max_iter-1, size(idx_Missing, 2));
+nLag = 3;
+nTask = length(series);
+tTrain = 137+nLag;
+tTest = tLen - tTrain;
 % Create the matrices
-for i = 1:size(idx_Missing, 2)
-    testIndex = idx_Missing(:, 1);
+A = zeros(nLoc, nLoc*nLag, nTask);
+X = cell(nTask, 1);
+Y = cell(nTask, 1);
+test.X = cell(nTask, 1);
+test.Y = cell(nTask, 1);
+
+mu = logspace(-1, 1.3, 10);
+
+ep = 1e-10;
+max_iter = 151;
+quality = zeros(max_iter-1, length(mu));
+for m = 1:length(mu)
+    U = chol(eye(nLoc) + mu(m)*sim);
+    for i = 1:nTask
+        Y{i} = series{i}(:, nLag+1:tTrain);
+        Y{i} = (U')\Y{i};               % This is the transformation
+        test.Y{i} = series{i}(:, tTrain-nLag+1:tLen);
+        X{i} = zeros(nLag*nLoc, (tTrain - nLag));
+        for ll = 1:nLag
+            X{i}(nLoc*(ll-1)+1:nLoc*ll, :) = series{i}(:, nLag+1-ll:tTrain-ll);
+            test.X{i}(nLoc*(ll-1)+1:nLoc*ll, :) = series{i}(:, tTrain-nLag+1-ll:tLen-ll);
+        end
+    end
     
-    index = ones(nLoc, 1);
-    index(testIndex) = 0;
-    Iomega = diag( index);
-    
-    ep = 1e-10;
-    mu = logspace(0, 2, 10);
-    quality(:, i) =  prepareData(series, Iomega, 5, sim, max_iter, ep, testIndex);
-    disp(i)
+    [~, tmp] = solveGreedyOrth(Y, X, ep, max_iter, U, test);
+    quality(:, m) = tmp(:, 2);
 end
-
-save('KrigingOrthoMultiIndex.mat', 'quality')
+save('ForecastingOrtho.mat', 'quality')
 % save('krigingOrtho.mat', 'quality')
-
