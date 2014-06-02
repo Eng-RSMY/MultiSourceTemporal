00dca9e37c77df5399464a7ecbfe3d315951d0a8
Rose Yu <yuqi.rose@hotmail.com>
2014-06-02 07:38:31 -0700

    Continue Tucker Synth Test

--- runTucker.m
+++ runTucker.m
@@ -7,7 +7,7 @@ global verbose;
 verbose = 1;
 tLen = [10, 50, 100, 200];
 qTucker = cell(4,1);
-for t = 1:length(tLen)
+for t = 4%1:length(tLen)
     qTucker{t} = zeros(10, 6); 
     parfor i = 1:10
         name = sprintf('synth%d_%d.mat', tLen(t), i);
